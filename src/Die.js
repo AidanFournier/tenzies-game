ae9f584992633c96cc8226fbcc8d4c5b7803cd0a
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-06-03 23:55:37 +0900

    Styled die component

--- src/Die.js
+++ src/Die.js
@@ -1,9 +1,10 @@
 import React from "react"
 
-export default function Die() {
+export default function Die(props) {
 
     return (
-        <div>
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
