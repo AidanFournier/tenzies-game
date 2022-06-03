c39080bbc36baa1a7dc573bbce0ffac9a0d3a169
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-06-04 00:03:37 +0900

    Created function to generate random dice number

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import Confetti from "react-confetti"
 import Die from "./Die"
 
 export default function App() {
-    return (
+    
+  function allNewDice() {
+    const newDice = []
+      for (let i = 0; i < 10; i++) {
+          newDice.push(Math.ceil(Math.random() * 6))
+      }
+      return newDice
+  }
+  return (
       <main>
         <div className="dice-container">
             <Die value="1" />
