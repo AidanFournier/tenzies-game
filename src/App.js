b722e25211a3920806a7946d9aca67fa7d495474
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-07-26 21:11:36 +0900

    Changed newDice into array of objects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Confetti from "react-confetti"
+import {nanoid} from "nanoid"
+
 import Die from "./Die"
 
 export default function App() {
@@ -8,7 +10,11 @@ export default function App() {
   function allNewDice() {
     const newDice = []
       for (let i = 0; i < 10; i++) {
-          newDice.push(Math.ceil(Math.random() * 6))
+          newDice.push({ 
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+            })
       }
       return newDice
   }
@@ -17,7 +23,9 @@ export default function App() {
     setDice(allNewDice());
   };
 
-  const diceElements = dice.map(die => <Die value={die} />)
+  const diceElements = dice.map(die => 
+    (<Die key={die.id} value={die.value} />
+  ))
 
   return (
       <main>
