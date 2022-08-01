2f8db1928710a8c6f0245583d2395f4fc87da47a
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-08-01 22:52:05 +0900

    Updated rollDice function

--- src/App.js
+++ src/App.js
@@ -7,20 +7,28 @@ import Die from "./Die"
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice())
 
+  function generateNewDie() {
+    return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+  }   
+
   function allNewDice() {
     const newDice = []
       for (let i = 0; i < 10; i++) {
-          newDice.push({ 
-            value: Math.ceil(Math.random() * 6),
-            isHeld: false,
-            id: nanoid()
-            })
+          newDice.push(generateNewDie())
       }
       return newDice
   }
 
   function rollDice() {
-    setDice(allNewDice());
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? 
+        die : 
+        generateNewDie()
+    }));
   };
 
   function holdDice(id) {
@@ -37,6 +45,8 @@ export default function App() {
 
   return (
       <main>
+        <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="dice-container">
           {diceElements}
         </div>
